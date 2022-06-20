3e91b48db722c325bfc38993a7e38a96ddacfff1
Adorn Choga <adornchoga@gmail.com>
2022-06-20 20:35:30 +0200

    Create provider schema

---
+++ server/models/provider.js
@@ -0,0 +1,7 @@
+import mongoose from 'mongoose';
+
+const providerSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+});
+
+export default mongoose.model('Provider', providerSchema);
