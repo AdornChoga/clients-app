8190da65919558d79b47e84ca48e12d10db8adea
Adorn Choga <adornchoga@gmail.com>
2022-06-21 07:01:04 +0200

    Setup swagger

--- server/index.js
+++ server/index.js
@@ -2,10 +2,33 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 import clients from './routes/clients.route.js';
 import providers from './routes/providers.route.js';
 
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Clients App API',
+      version: '1.0.0',
+      description: 'This is an API for clients app',
+    },
+    servers: [
+      {
+        url: 'http://localhost:5000',
+      },
+    ],
+  },
+  apis: ['./documentation/*.js'],
+};
+
+const specs = swaggerJSDoc(options);
+
 const app = express();
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 
 app.use(cors());
 
