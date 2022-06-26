6d5327a102bcd1d05d1e133394f44a81a5fb01d4
Adorn Choga <adornchoga@gmail.com>
2022-06-26 10:26:02 +0200

    Add uniquess constraint on provider name

--- server/models/provider.js
+++ server/models/provider.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const providerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
 });
 
 export default mongoose.model('Provider', providerSchema);
