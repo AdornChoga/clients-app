4d5e954447fbab2b5fc11f77d49ba7995ff98e63
Adorn Choga <adornchoga@gmail.com>
2022-06-27 17:32:20 +0200

    Add timestamps to clients

--- server/models/client.js
+++ server/models/client.js
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
-const clientSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true, index: true },
-  phone: { type: String, required: true },
-  providers: [
-    { _id: { type: mongoose.SchemaTypes.ObjectId, ref: 'Provider' } },
-  ],
-});
+const clientSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, index: true },
+    phone: { type: String, required: true },
+    providers: [
+      { _id: { type: mongoose.SchemaTypes.ObjectId, ref: 'Provider' } },
+    ],
+  },
+  { timestamps: true },
+);
 
 export default mongoose.model('Client', clientSchema);
